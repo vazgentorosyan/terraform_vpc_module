af7f3d1bb3fe35e15838246899baf05cb01da000
Vazgen Torosyan <vazgen.torosyan@railsbank.com>
2021-10-27 13:50:03 +0400

    added new ec2 in nat subnet

--- ec-2.tf
+++ ec-2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "nat_ec2" {
+resource "aws_instance" "public_ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   subnet_id = aws_subnet.PublicSubnet.0.id
@@ -7,9 +7,24 @@ resource "aws_instance" "nat_ec2" {
   security_groups = [aws_security_group.public_sg.id]
 
   tags = {
-    Name = upper("${var.product_name}-EC2")
+    Name = upper("${var.product_name}-PUBLIC-EC2")
   }
   lifecycle {
-    ignore_changes = [security_groups]
+    ignore_changes = [security_groups, tags]
+  }
+}
+
+resource "aws_instance" "nat_ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.NatSubnet.0.id
+  key_name = aws_key_pair.key_pair.key_name
+  security_groups = [aws_security_group.nat_sg.id]
+
+  tags = {
+    Name = upper("${var.product_name}-NAT-EC2")
+  }
+  lifecycle {
+    ignore_changes = [security_groups, tags]
   }
 }
